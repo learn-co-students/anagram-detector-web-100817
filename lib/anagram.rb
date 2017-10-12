7f64e06bb49366a8a894a3a0a0c4c304c17a69e8
slin12 <s.lin12@gmail.com>
2017-10-12 17:38:26 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,12 @@
 # Your code goes here!
+class Anagram
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def match(words_array)
+    words_array.select{|word| word.chars.sort == self.name.chars.sort}
+  end
+end
